babd42b62f891289e48bdd112bc47fd50faa0125
CaretParrot <87083627+CaretParrot@users.noreply.github.com>
2024-03-12 21:12:04 -0500

    Added parser to the idTree remove function script.js

--- script.js
+++ script.js
@@ -118,8 +118,7 @@ function playbackTake(take) {
 
 function deleteVideo() {
     takeList.splice(currentVideo - 1, 1);
-    console.log(currentVideo);
-    idTree[currentVideo].remove();
+    idTree[`${currentVideo}`].remove();
     idTree.source.src = undefined;
     idTree.video.muted = true;
     idTree.video.controls = false;
